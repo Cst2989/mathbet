48e9edd0197e3d0be77a1b5de9d9e577665cbf71
Neciu Dan <neciudan@Necius-MacBook-Pro.local>
2017-04-06 16:16:26 +0300

    added league data

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,21 @@
-import { Component } from '@angular/core';
-
+import { Component, OnInit} from '@angular/core';
+import { AppService } from './app.service';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'app works!';
+export class AppComponent implements OnInit {
+
+  public title: string = 'app works!';
+
+  constructor(private appService: AppService) {
+  }
+
+  public ngOnInit(): void {
+
+    this.appService.load().subscribe(r => {
+        console.log(r)
+    })
+  }
 }
