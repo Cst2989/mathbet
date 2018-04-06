ceac353c62be87a56b01e8ff98b61a7576b8682e
Neciu Dan <cst2989@gmail.com>
2018-04-06 22:18:22 +0300

    Added drag and drop instead of select to see multiple values

--- src/app/mathbet/containers/league.component.ts
+++ src/app/mathbet/containers/league.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { Router, ActivatedRoute } from '@angular/router';
 import { MathService } from '../mathbet.service';
-import { FormGroup, FormControl, Validators } from '@angular/forms';
+import { FormArray, FormGroup, FormControl, Validators } from '@angular/forms';
 
 @Component({
     selector: 'league',
@@ -15,7 +15,7 @@ export class LeagueComponent implements OnInit {
   public isSubmited: boolean = false;
   public showInfo: boolean = false;
   public data: any;
-
+  public matchesNr: number;
   public allTeams: any;
 
   public myGroup: FormGroup;
@@ -25,10 +25,10 @@ export class LeagueComponent implements OnInit {
   public homeWin: number;
   public awayWin: number;
   public draw: number;
-
-
+  public form: FormGroup;
+  public matches: any;
   public leagueAtackStrength: number;
-
+  public dragMatch: any;
   constructor(
     private router: Router,
     private mathService: MathService,
@@ -46,49 +46,77 @@ export class LeagueComponent implements OnInit {
           this.showInfo = false;
           this.data = r;
           this.allTeams = this.data.getTeams();
+          this.matchesNr = this.allTeams.length / 2;
           this.isLoaded = true;
-          this.myGroup = new FormGroup({
-            homeTeam: new FormControl('', Validators.required),
-            awayTeam: new FormControl('', Validators.required)
+
+          this.form = new FormGroup({
+            "matches": new FormArray([])
           });
+
+          this.matches = this.form.get('matches') as FormArray;
+
+          for (let i = 0; i < this.matchesNr; i++) {
+              let myGroup = new FormGroup({
+                homeTeam: new FormControl(''),
+                awayTeam: new FormControl(''),
+                homeWin: new FormControl(0),
+                draw: new FormControl(0),
+                awayWin: new FormControl(0),
+                over: new FormControl(0),
+                under: new FormControl(0),
+              });
+              this.matches.push(myGroup);
+          }
 
         })
   }
 
-  public onSubmit(): void {
-    this.isSubmited = true;
+  public drag(ev, team) {
+    this.dragMatch = team;
+  }
 
-    if (!this.myGroup.valid) return;
-    this.showInfo = true;
-    let HomeTeam = this.myGroup.value.homeTeam;
-    let AwayTeam = this.myGroup.value.awayTeam;
+  public dragOver(ev) {
+    ev.preventDefault();
+  }
 
-    let leagueHomeStrength = this.data.leagueAtackStrength();
-    let leagueAwayStrength = this.data.leagueDefenseStrength();
+  public drop(ev, location, index) {
+    this.form.get('matches').get(index.toString()).get(location).setValue(this.dragMatch);
 
-    let homeAt = this.data.getHomeAtack(HomeTeam);
-    let homeDef = this.data.getHomeDefense(HomeTeam);
+    let HomeTeam = this.form.get('matches').get(index.toString()).get('homeTeam').value;
+    let AwayTeam = this.form.get('matches').get(index.toString()).get('awayTeam').value;
+    if (HomeTeam && AwayTeam) {
+      //move to top
+      let leagueHomeStrength = this.data.leagueAtackStrength();
+      let leagueAwayStrength = this.data.leagueDefenseStrength();
 
-    let awayAt = this.data.getAwayAtack(AwayTeam);
-    let awayDef = this.data.getAwayDefense(AwayTeam);
+      let homeAt = this.data.getHomeAtack(HomeTeam);
+      let homeDef = this.data.getHomeDefense(HomeTeam);
 
-    let predictedHomeGoals = homeAt * awayDef * leagueHomeStrength;
-    let predictedAwayGoals = awayAt * homeDef * leagueAwayStrength;
+      let awayAt = this.data.getAwayAtack(AwayTeam);
+      let awayDef = this.data.getAwayDefense(AwayTeam);
 
-    let home0 = this.probabilityCalc(0, predictedHomeGoals);
-    let home1 = this.probabilityCalc(1, predictedHomeGoals);
-    let home2 = this.probabilityCalc(2, predictedHomeGoals);
+      let predictedHomeGoals = homeAt * awayDef * leagueHomeStrength;
+      let predictedAwayGoals = awayAt * homeDef * leagueAwayStrength;
 
-    let away0 = this.probabilityCalc(0, predictedAwayGoals);
-    let away1 = this.probabilityCalc(1, predictedAwayGoals);
-    let away2 = this.probabilityCalc(2, predictedAwayGoals);
+      let home0 = this.probabilityCalc(0, predictedHomeGoals);
+      let home1 = this.probabilityCalc(1, predictedHomeGoals);
+      let home2 = this.probabilityCalc(2, predictedHomeGoals);
 
-    this.homeWin = this.data.getHomeWin(HomeTeam, AwayTeam);
-    this.awayWin = this.data.getAwayWin(HomeTeam, AwayTeam);
-    this.draw = this.data.getDraw(HomeTeam, AwayTeam);
-    this.underValue = ((home0*away0) + (home1*away0) + (home0*away1) + (home1*away1) + (home2*away0) + (home0*away2)) * 100;
-    this.overValue= 100 - this.underValue ;
+      let away0 = this.probabilityCalc(0, predictedAwayGoals);
+      let away1 = this.probabilityCalc(1, predictedAwayGoals);
+      let away2 = this.probabilityCalc(2, predictedAwayGoals);
 
+      this.form.get('matches').get(index.toString()).get('homeWin').setValue(this.data.getHomeWin(HomeTeam, AwayTeam));
+      this.form.get('matches').get(index.toString()).get('awayWin').setValue(this.data.getAwayWin(HomeTeam, AwayTeam));
+      this.form.get('matches').get(index.toString()).get('draw').setValue(this.data.getDraw(HomeTeam, AwayTeam));
+
+      let under = ((home0*away0) + (home1*away0) + (home0*away1) + (home1*away1) + (home2*away0) + (home0*away2)) * 100;
+      let over = 100 - under ;
+
+      this.form.get('matches').get(index.toString()).get('over').setValue(over);
+      this.form.get('matches').get(index.toString()).get('under').setValue(under);
+
+    }
   }
 
   private factorial(number){
